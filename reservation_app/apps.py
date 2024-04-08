cc4b5507058372c52c4cd045b675579dc0f46170
muqimbayev <muqimbayev@gmail.com>
2024-04-08 16:11:03 +0500

    reservation

--- reservation_app/apps.py
+++ reservation_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ReservationAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reservation_app'
+    verbose_name = 'Reservation section'
