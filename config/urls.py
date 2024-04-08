8ca34ffcc2ffa40ee6ef0e1824980b46f7e116d8
muqimbayev <muqimbayev@gmail.com>
2024-04-08 16:23:31 +0500

    urls

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@
 URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('main_app.urls')),
+    path('food/', include('food_app.urls')),
+    path('reserve/', include('reservation_app.urls')),
+    path('blog/', include('blog_app.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
